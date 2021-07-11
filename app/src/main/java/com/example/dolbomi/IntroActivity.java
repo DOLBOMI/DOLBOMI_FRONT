3bb944a3814034453a1f80dc470fbafbcb331945
Woosung Kim <woosung0420k@naver.com>
2021-07-11 19:15:16 +0900

    [#7] 홈 메인 생성 (#9)
    * feat: 홈 메인 생성 (#7)
    
    * fix: 홈 메인 파일명 수정 (#7)
    
    * fix: [#6]conflict 해결

---
+++ app/src/main/java/com/example/dolbomi/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.example.dolbomi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_intro);
+
+        IntroThread introThread = new IntroThread(handler);
+        introThread.start();
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/example/dolbomi/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.dolbomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import com.github.mikephil.charting.charts.BarChart;
+
+public class MainActivity extends AppCompatActivity {
+    BarChart barChart;
+    int x = 7330;
+
+    //public void BarChartGraph(ArrayList<String> labelList, ArrayList<Integer> valList) {
+    public void BarChartGraph() {
+        // BarChart 메소드
+
+        barChart=findViewById(R.id.bar_chart);
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1, x));
+        entries.add(new BarEntry(0, 10000));
+
+        /*
+        for(int i=0; i < valList.size();i++){
+            entries.add(new BarEntry((Integer) valList.get(i), i));
+        }
+        */
+
+        BarDataSet depenses = new BarDataSet (entries, "권장 걸음 수 / 금일 걸음 수 "); // 변수로 받아서 넣어줘도 됨
+        depenses.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        //barChart.setDescription();
+
+        /*
+        ArrayList<String> labels = new ArrayList<String>();
+        for(int i=0; i < labelList.size(); i++){
+            labels.add((String) labelList.get(i));
+        }
+         */
+
+        BarData data = new BarData(depenses);
+        //BarData data = new BarData(labels,depenses); // 라이브러리 v3.x 사용하면 에러 발생함
+        depenses.setColors(ColorTemplate.LIBERTY_COLORS); //
+
+
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setDrawAxisLine(false);
+        barChart.getXAxis().setEnabled(false);
+
+
+        barChart.setData(data);
+        barChart.animateXY(1000,1000);
+        barChart.invalidate();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+
+        Log.d(this.getClass().getName(), (String)textView.getText());
+
+
+        textView.setText(x+"걸음\n"+(x*0.37)+"Kcal");
+
+        BarChartGraph();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu) ;
+
+        return true ;
+    }
+
+}
