75faac7b309ba3dc9ab856bedc0370cbc7d98397
enyung <45346786+eyl056@users.noreply.github.com>
2021-07-11 16:12:37 +0900

    feat: rename activity, delete splash

--- app/src/main/java/com/example/dolbomi/LoginActivity.java
+++ app/src/main/java/com/example/dolbomi/LoginActivity.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
 
         //회원가입 버튼 클릭시 액티비티 전환
--- app/src/main/java/com/example/dolbomi/RegisterActivity.java
+++ app/src/main/java/com/example/dolbomi/RegisterActivity.java
@@ -19,7 +19,7 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
