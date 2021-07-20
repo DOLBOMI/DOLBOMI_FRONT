7336700692865a4d6a489272c0835f999977cf80
Yujin <55319788+yujinHan97@users.noreply.github.com>
2021-07-20 15:45:16 +0900

    [#13] 관리자 모드 메인 페이지 (#20)
    * feat: 관리자 모드 메인 페이지 생성
    
    * fix: 코드 리뷰 해결

---
+++ app/src/main/java/com/example/dolbomi/ChangeStatus.java
@@ -0,0 +1,14 @@
+package com.example.dolbomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_status);
+    }
+}
