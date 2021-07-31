4ed1bf36fc2aa3cdfa76ce46d0df05c81ba6ea06
sieun <31584255+sileeee@users.noreply.github.com>
2021-07-31 13:51:19 +0900

    [#22] 페이지 연결 (#24)
    * fix: 내 집 상태 화면 리팩토링
    
    * fix: 내 집 상태 화면 리팩토링
    
    * fix: 메인 화면 리팩토링
    
    * fix: 메인 화면 리팩토링
    
    * feat: 페이지 연결
    
    * feat: 메인화면에서의 버튼 링크 연결
    
    * fix: [#16]conflict 해결
    
    Co-authored-by: Yujin <55319788+yujinHan97@users.noreply.github.com>
    Co-authored-by: Woosung Kim <woosung0420k@naver.com>
    Co-authored-by: enyung <45346786+eyl056@users.noreply.github.com>

--- app/src/main/java/com/example/dolbomi/MainActivity.java
+++ app/src/main/java/com/example/dolbomi/MainActivity.java
@@ -1,11 +1,27 @@
 package com.example.dolbomi;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.PatternMatcher;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -17,6 +33,7 @@ import java.util.ArrayList;
 import com.github.mikephil.charting.charts.BarChart;
 
 public class MainActivity extends AppCompatActivity {
+    Dialog dialog;
     BarChart barChart;
     int x = 7330;
 
@@ -29,32 +46,26 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new BarEntry(1, x));
         entries.add(new BarEntry(0, 10000));
 
-        /*
-        for(int i=0; i < valList.size();i++){
+        /*for(int i=0; i < valList.size();i++){
             entries.add(new BarEntry((Integer) valList.get(i), i));
-        }
-        */
+        }*/
 
         BarDataSet depenses = new BarDataSet (entries, "권장 걸음 수 / 금일 걸음 수 "); // 변수로 받아서 넣어줘도 됨
         depenses.setAxisDependency(YAxis.AxisDependency.RIGHT);
         //barChart.setDescription();
 
-        /*
-        ArrayList<String> labels = new ArrayList<String>();
+        /*ArrayList<String> labels = new ArrayList<String>();
         for(int i=0; i < labelList.size(); i++){
             labels.add((String) labelList.get(i));
-        }
-         */
+        }*/
 
         BarData data = new BarData(depenses);
         //BarData data = new BarData(labels,depenses); // 라이브러리 v3.x 사용하면 에러 발생함
         depenses.setColors(ColorTemplate.LIBERTY_COLORS); //
 
-
         barChart.getXAxis().setDrawGridLines(false);
         barChart.getXAxis().setDrawAxisLine(false);
         barChart.getXAxis().setEnabled(false);
-
 
         barChart.setData(data);
         barChart.animateXY(1000,1000);
@@ -69,6 +80,14 @@ public class MainActivity extends AppCompatActivity {
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
 
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle("83세 한유진");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFFFF"));
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
         TextView textView = (TextView)findViewById(R.id.textView);
 
         Log.d(this.getClass().getName(), (String)textView.getText());
@@ -77,15 +96,143 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(x+"걸음\n"+(x*0.37)+"Kcal");
 
         BarChartGraph();
+
+        Button lifePatternButton = (Button) findViewById(R.id.lifePatternButton);
+        Button tagHistoryButton = (Button) findViewById(R.id.tagHistoryButton);
+        Button MyHomeButton = (Button) findViewById(R.id.MyHomeButton);
+        Button SensorButton = (Button) findViewById(R.id.SensorButton);
+
+        lifePatternButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), LifePatternActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tagHistoryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), TagHistory.class);
+                startActivity(intent);
+            }
+        });
+
+        MyHomeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), HomeStatus.class);
+                startActivity(intent);
+            }
+        });
+
+        SensorButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), SensorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar_actions, menu) ;
+        getMenuInflater().inflate(R.menu.main_toolbar, menu) ;
 
         return true ;
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_call:
+                // User chose the "Settings" item, show the app settings UI...
+
+                dialog = new Dialog(MainActivity.this);       // Dialog 초기화
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+                dialog.setContentView(R.layout.activity_call);             // xml 레이아웃 파일과 연결
+
+                dialog.show();
+
+                Button cancel = dialog.findViewById(R.id.button8);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // 원하는 기능 구현
+                        dialog.dismiss(); // 다이얼로그 닫기
+                    }
+                });
+
+                /*Button call_guardian = dialog.findViewById(R.id.imageButton2);
+                call_guardian.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // 원하는 기능 구현
+                        TextView a=dialog.findViewById(R.id.textView8);
+                        show(a.getText().toString());
+                    }
+                });
+
+                Button call_admin = dialog.findViewById(R.id.imageButton3);
+                call_admin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // 원하는 기능 구현
+                        TextView a=dialog.findViewById(R.id.textView11);
+                        show(a.getText().toString());
+                    }
+                });
+                 */
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(getApplicationContext(), "메뉴 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void show(String str) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("전화 걸기\n");
+        builder.setMessage(str + "에게 전화를 거시겠습니까?");
+
+        AlertDialog b_alert=builder.create();
+
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), str + "에게 전화 걸기", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                b_alert.dismiss();
+            }
+        });
+        b_alert.show();
+    }
+
+    public void ClickSensor(View view) {
+        Intent intent = new Intent( this, SensorActivity.class);
+        startActivity(intent);
+    }
+
+    public void ClickHomeStatus(View view) {
+        Intent intent = new Intent( this, HomeStatus.class);
+        startActivity(intent);
+    }
+
+    public void ClickPattern(View view) {
+        Intent intent = new Intent( this, LifePatternActivity.class);
+        startActivity(intent);
+    }
+
+    public void ClickTagHistory(View view) {
+        Intent intent = new Intent( this, TagHistory.class);
+        startActivity(intent);
+    }
 }
