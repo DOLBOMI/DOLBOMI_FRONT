e9719a03ff539d1faf4a4ea8311e3a56d18ae496
enyung <45346786+eyl056@users.noreply.github.com>
2021-08-03 01:45:31 +0900

    feat: 버튼 요일 나타내기

--- app/src/main/java/com/example/dolbomi/LifePatternActivity.java
+++ app/src/main/java/com/example/dolbomi/LifePatternActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -18,6 +21,8 @@ import com.google.firebase.database.ValueEventListener;
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -30,11 +35,15 @@ public class LifePatternActivity extends AppCompatActivity {
     int month_ = date.get(Calendar.MONTH) + 1;
     int day_ = date.get(Calendar.DAY_OF_MONTH);
     int hour_ = date.get(Calendar.HOUR_OF_DAY) + 9;
+    LocalDate today;
+    DayOfWeek dayOfWeek;
+    int dayOfWeekNumber;
     String month_s;
     String day_s;
 
     String todayDate;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,9 +51,17 @@ public class LifePatternActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        today = LocalDate.of(year_, month_, day_);
+        dayOfWeek  = today.getDayOfWeek();
+        dayOfWeekNumber = dayOfWeek.getValue();
+
         if (hour_ >= 24) {
             hour_ -= 24;
             day_ += 1;
+            dayOfWeekNumber += 1;
+            if (dayOfWeekNumber == 8) {
+                dayOfWeekNumber = 1;
+            }
         }
 
         if(month_ < 10) {
@@ -56,6 +73,8 @@ public class LifePatternActivity extends AppCompatActivity {
 
         todayDate = year_ + "-" + month_s + "-" + day_s;
 
+
+
         getLifePatternValue();
 
 
@@ -63,6 +82,44 @@ public class LifePatternActivity extends AppCompatActivity {
 
     private void getLifePatternValue() {
         setContentView(R.layout.activity_life_pattern);
+
+        Button monButton = (Button) findViewById(R.id.monButton);
+        Button tueButton = (Button) findViewById(R.id.tueButton);
+        Button wedButton = (Button) findViewById(R.id.wedButton);
+        Button thuButton = (Button) findViewById(R.id.monButton);
+        Button friButton = (Button) findViewById(R.id.friButton);
+        Button satButton = (Button) findViewById(R.id.satButton);
+        Button sunButton = (Button) findViewById(R.id.sunButton);
+
+        if (dayOfWeekNumber == 1) {
+            // 월요일
+            monButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            monButton.setTextColor(Color.BLACK);
+        } else if (dayOfWeekNumber == 2) {
+            // 화요일
+            tueButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            tueButton.setTextColor(Color.BLACK);
+        } else if (dayOfWeekNumber == 3) {
+            // 수요일
+            wedButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            wedButton.setTextColor(Color.BLACK);
+        } else if (dayOfWeekNumber == 4) {
+            // 목요일
+            thuButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            thuButton.setTextColor(Color.BLACK);
+        } else if (dayOfWeekNumber == 5) {
+            // 금요일
+            friButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            friButton.setTextColor(Color.BLACK);
+        } else if (dayOfWeekNumber == 6) {
+            // 토요일
+            satButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            satButton.setTextColor(Color.BLACK);
+        } else if (dayOfWeekNumber == 7) {
+            // 일요일
+            sunButton.setBackground(Drawable.createFromPath("@drawable/today_button"));
+            sunButton.setTextColor(Color.BLACK);
+        }
 
         TextView dateTextView = (TextView) findViewById(R.id.dataTextView);
         // 오늘 날짜 보이기
