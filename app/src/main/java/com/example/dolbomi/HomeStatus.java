53cca24b7332456b67d8d4b445bc2164ab9e060f
yujinHan97 <55319788+yujinHan97@users.noreply.github.com>
2021-08-08 15:15:56 +0900

    feat: 내 집 상태 페이지 센서 데이터 적용

--- app/src/main/java/com/example/dolbomi/HomeStatus.java
+++ app/src/main/java/com/example/dolbomi/HomeStatus.java
@@ -1,54 +1,45 @@
 package com.example.dolbomi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.graphics.Color;
+import android.graphics.fonts.Font;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.data.Entry;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class HomeStatus extends AppCompatActivity {
-    private LineChart chart;
-
-    public void lineGraph() {
-        chart = findViewById(R.id.chart);
-        ArrayList<Entry> values = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            float val = (float) (Math.random() * 10);
-            values.add(new Entry(i, val));
-        }
-
-        LineDataSet set1;
-        set1 = new LineDataSet(values, "DataSet 1");
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1); // add the data sets
-
-        // create a data object with the data sets
-        LineData data = new LineData(dataSets);
-
-        // black lines and points
-        set1.setColor(Color.BLACK);
-        set1.setCircleColor(Color.BLACK);
-
-        // set data
-        chart.setData(data);
-        chart.invalidate();
-    }
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status_home);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        getValue();
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         toolbar.setTitle("내 집 상태");
@@ -58,32 +49,67 @@ public class HomeStatus extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
-
-        Button temp_btn = (Button) findViewById(R.id.button1);
-        Button moist_btn = (Button) findViewById(R.id.button2);
-        Button gas_btn = (Button) findViewById(R.id.button3);
-
-        temp_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                lineGraph();
-            }
-        });
-        moist_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                lineGraph();
-            }
-        });
-        gas_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                //lineGraph();
-            }
-        });
-
-
     }
 
+    private void getValue() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd");
+        String getTime = simpleDate.format(date);
 
+        final DatabaseReference temperature = mDatabase.child("Home1").child(getTime).child("temperature");
+        final DatabaseReference humidity = mDatabase.child("Home1").child(getTime).child("humidity");
+        final DatabaseReference gas = mDatabase.child("Home1").child(getTime).child("gas");
+
+        setContentView(R.layout.status_home);
+        TextView temp_text = (TextView) findViewById(R.id.temptext);
+        TextView humid_text = (TextView) findViewById(R.id.humidtext);
+        TextView gas_text = (TextView) findViewById(R.id.gastext);
+
+        temperature.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String temp = "온도 " + snapshot.getValue().toString() + "°C";
+                temp_text.setText(temp);
+
+                //if (snapshot.exists()) { }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        humidity.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String humidity = "습도 " + snapshot.getValue().toString() + "%";
+                humid_text.setText(humidity);
+
+                //if (snapshot.exists()) { }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        gas.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String gas = "가스 농도 " + snapshot.getValue().toString() ;
+                gas_text.setText(gas);
+
+                //if (snapshot.exists()) { }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
 }
